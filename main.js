fce8b75bb911c9e105b67897c618fcab6f6fd997
Iara1996 <iara.iara@outlook.it>
2024-12-04 02:54:37 +0100

    Add conditionals statements for different commisions

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ Supponiamo per semplicità che ogni progetto richieda lo stesso numero di ore di
 /*L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti: YHDNU32,
 JANJC63, PWKCN25, SJDPO96, POCIE24. */ 
 
-
 /*Se il codice promozionale è valido, l'utente ha diritto ad uno sconto del 25% sul
 prezzo finale. Se il codice inserito non è valido, il sito deve informare l’utente che il codice
 non è valido e il prezzo finale viene calcolato senza applicare sconti.
@@ -24,8 +23,8 @@ Il risultato del calcolo del prezzo finale deve essere visualizzato in “forma 
 decimali e il simbolo dell’euro). */ 
 
 // salvare in una costante gli elementi del DOM che competono al risultato
-const workElement = document.getElementById('inputWork').value;
-const promotionalCodesElement = document.getElementById('inputPromotionalCodes').value;
+const workElement = document.getElementById ('inputWork');
+const promotionalCodesElement = document.getElementById('inputPromotionalCodes');
 
 //salvare in una costante le ore di lavoro necessarie
 const timePerProject = 10;
@@ -35,6 +34,31 @@ const backendDevelopPerHour = 20.50;
 const frontendDevelopPerHour = 15.30;
 const projectAnalysisPerHour = 33.60;
 
+// salvare in un array i codici sconto che il cliente potrebbe utilizzare
+let promotionalCodes = ['YHDNU32','JANJC63','PWKCN25','SJDPO96','POCIE24'];
+
 //salvare in una costante l'elemento form con id estimateRequest da utilizzare nella funzione
 const estimateRequestElement = document.getElementById('estimateRequest');
 
+
+estimateRequestElement.addEventListener('submit', function(event) {
+    event.preventDefault(); //blocca refresh continuo della pagina
+    let selectedInputWork = parseInt(workElement.value);
+    const discount = (promotionalCodesElement.value).toUpperCase();
+
+    console.log(selectedInputWork, discount);
+
+    if(selectedInputWork === 1){
+        selectedInputWork = (backendDevelopPerHour * timePerProject).toFixed(2);
+    }
+    else if(selectedInputWork === 2){
+        selectedInputWork = (frontendDevelopPerHour * timePerProject).toFixed(2);
+    }
+    else if (selectedInputWork === 3) {
+        selectedInputWork = (projectAnalysisPerHour * timePerProject).toFixed(2);
+    }
+        
+    console.log(selectedInputWork);
+
+}
+)
